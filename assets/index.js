127ba63e1fb34548d870ade05b131490ed2237da
Lubov <lubaparfyonova@gmail.com>
2022-02-18 19:35:13 +0200

    Create WrpperElement

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,51 @@
 'use strict';
+
+//React
+//ReactDOM
+
+/* const reactElement = React.createElement('h1', {
+    title: 'hello', 
+    className: 'heading'},
+     'Hello, React.js!'); */
+
+/* 
+const pElement = React.createElement('p', {title: 'p'}, 'TEXT text TEXT','TEXT text TEXT','TEXT text TEXT' );
+const articleElement = React.createElement('article', {title: 'article'}, 'This is article.', pElement);
+
+ReactDOM.render(articleElement, WHERE);
+ */
+
+class Heading extends React.Component {
+    render() {
+        console.log(this.props);
+        const {titleForHeading, classForHeading, children} = this.props;
+        return React.createElement('h1', {
+            title: titleForHeading,
+            className: classForHeading
+        }, 
+        ...children);//'Class heading'
+    }
+}
+const reactElement = React.createElement(Heading, {
+    titleForHeading: 'Now Im here',
+    classForHeading: 'heading'},
+    'str1');
+const reactElement2 = React.createElement(Heading, {
+    classForHeading: 'new-heading'
+}, 'New heading')
+
+const reactWrapperElement = React.createElement('div' , null, reactElement, reactElement2);
+
+ReactDOM.render(reactWrapperElement, document.getElementById('root'));
+/* ReactDOM.render([reactElement, reactElement2], document.getElementById('root')); */ // bad practise
+
+/* class Article extends React.Component{
+    render() {
+        return React.createElement('article', {
+            title: 'Article'
+        },
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis accusantium velit sint excepturi! Aspernatur animi officia deleniti voluptatem cumque, quibusdam ex optio hic, fugiat labore quaerat eum error! Asperiores, magnam. ');
+    }
+} */
+/* const articleElement = React.createElement(Article);
+ReactDOM.render([reactElement, articleElement], document.getElementById('root')) */
